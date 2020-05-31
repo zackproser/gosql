9ee04a0484c5fc953dc87e65c1debd446f35ce49
Zack Proser <zackproser@gmail.com>
2020-05-30 19:07:34 -0700

    Implement numeric and char delimited lexing

--- lexer.go
+++ lexer.go
@@ -3,80 +3,78 @@ package main
 import "fmt"
 
 type location struct {
-	line uint 
-	col uint 
+	line uint
+	col  uint
 }
 
-type keyword string 
+type keyword string
 
 const (
 	selectKeyworkd keyword = "select"
-	fromKeyword keyword = "from"
-	asKeyword keyword = "as"
-	tableKeyword keyword = "table"
-	createKeyword keyword = "create"
-	insertKeyword keyword = "insert"
-	intoKeyword keyword = "into"
-	valuesKeyword keyword = "values"
-	intKeyword keyword = "int"
-	textKeyword keyword = "text"
+	fromKeyword    keyword = "from"
+	asKeyword      keyword = "as"
+	tableKeyword   keyword = "table"
+	createKeyword  keyword = "create"
+	insertKeyword  keyword = "insert"
+	intoKeyword    keyword = "into"
+	valuesKeyword  keyword = "values"
+	intKeyword     keyword = "int"
+	textKeyword    keyword = "text"
 )
 
-type symbol string 
+type symbol string
 
 const (
- semicolonSymbol symbol = ";"
- asteriskSymbol symbol = "*"
- commaSymbol symbol = ","
- leftparensSymbol symbol = "("
- rightparensSymbol symbol = ")"
+	semicolonSymbol   symbol = ";"
+	asteriskSymbol    symbol = "*"
+	commaSymbol       symbol = ","
+	leftparensSymbol  symbol = "("
+	rightparensSymbol symbol = ")"
 )
 
-type tokenKind uint 
+type tokenKind uint
 
 const (
-	keywordKind tokenKind = iota 
-	symbolKind 
-	identifierKind 
+	keywordKind tokenKind = iota
+	symbolKind
+	identifierKind
 	stringKind
 	numericKind
 )
 
 type token struct {
-	value string 
-	kind tokenKind
-	loc location
+	value string
+	kind  tokenKind
+	loc   location
 }
 
-
 type cursor struct {
-	pointer uint 
-	loc location 
+	pointer uint
+	loc     location
 }
 
 func (t *token) equals(other *token) bool {
 	return t.value == other.value && t.kind == other.kind
 }
 
-type lexer func(string, cursor) (*token, cursor bool)
+type lexer func(string, cursor) (*token, cursor, bool)
 
 func lex(source string) ([]*token, error) {
 	tokens := []*token{}
 	cur := cursor{}
 
-lex: 
+lex:
 	for cur.pointer < uint(len(source)) {
 		lexers := []lexer{lexKeyword, lexSymbol, lexString, lexNumeric, lexIdentifier}
 		for _, l := range lexers {
 			if token, newCursor, ok := l(source, cur); ok {
-				cur = newCursor 
+				cur = newCursor
 
-				// Omit nil tokens for valid, but empty syntax like newlines 
+				// Omit nil tokens for valid, but empty syntax like newlines
 				if token != nil {
 					tokens = append(tokens, token)
 				}
-
-				continue lex 
+				continue lex
 			}
 		}
 
@@ -86,4 +84,119 @@ lex:
 		}
 		return nil, fmt.Errorf("Unable to lex token %s, at %d:%d, hint, cur.loc.line, cur.loc.col")
 	}
+
+	return tokens, nil
+}
+
+func lexNumeric(source string, ic cursor) (*token, cursor, bool) {
+	cur := ic
+
+	periodFound := false
+	expMarkerFound := false
+
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c := source[cur.pointer]
+		cur.loc.col++
+
+		isDigit := c >= '0' && c <= '9'
+		isPeriod := c == '.'
+		isExpMarker := c == 'e'
+
+		// Must start with a digit or period
+		if cur.pointer == ic.pointer {
+			if !isDigit && !isPeriod {
+				return nil, ic, false
+			}
+
+			periodFound = isPeriod
+			continue
+		}
+
+		if isPeriod {
+			if periodFound {
+				return nil, ic, false
+			}
+
+			periodFound = true
+			continue
+		}
+
+		if isExpMarker {
+			if expMarkerFound {
+				return nil, ic, false
+			}
+
+			// No periods allowed after expMarker
+			periodFound = true
+			expMarkerFound = true
+
+			// expMarker must be followed by digits
+			if cur.pointer == uint(len(source)-1) {
+				return nil, ic, false
+			}
+
+			cNext := source[cur.pointer+1]
+			if cNext == '-' || cNext == '+' {
+				cur.pointer++
+				cur.loc.col++
+			}
+
+			continue
+		}
+
+		if !isDigit {
+			break
+		}
+	}
+
+	// No characters accumulated
+	if cur.pointer == ic.pointer {
+		return nil, ic, false
+	}
+
+	return &token{
+		value: source[ic.pointer:cur.pointer],
+		loc:   ic.loc,
+		kind:  numericKind,
+	}, cur, true
+}
+
+func lexCharacterDelimited(source string, ic cursor, delimiter byte) (*token, cursor, bool) {
+	cur := ic
+
+	if len(source[cur.pointer:]) == 0 {
+		return nil, ic, false
+	}
+
+	if source[cur.pointer] != delimiter {
+		return nil, ic, false
+	}
+
+	cur.loc.col++
+	cur.pointer++
+
+	var value []byte
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c := source[cur.pointer]
+
+		if c == delimiter {
+			// SQL escapes are via double characters, not backslash.
+			if cur.pointer+1 >= uint(len(source)) || source[cur.pointer+1] != delimiter {
+				return &token{
+					value: string(value),
+					loc:   ic.loc,
+					kind:  stringKind,
+				}, cur, true
+			} else {
+				value = append(value, delimiter)
+				cur.pointer++
+				cur.loc.col++
+			}
+		}
+
+		value = append(value, c)
+		cur.loc.col++
+	}
+
+	return nil, ic, false
 }
